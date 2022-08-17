145697e410d71f1270663debf3c1861160891726
muffin9 <jinlog9@gmail.com>
2022-08-17 23:21:53 +0800

    feat: 프로그래머스 문제 커밋.

--- programmers/구명보트.js
+++ programmers/구명보트.js
@@ -1,5 +1,20 @@
 function solution(people, limit) {
+    people.sort((a, b) => a - b);
+    let left = 0;
+    let right = people.length - 1;
+    let boat = 0;
+
+    while(left <= right) {
+        if(people[left] + people[right] <= limit) {
+            left += 1;
+            right -= 1;
+        } else {
+            right -=1;
+        }
+        boat += 1;
+    }
+    return boat;
 }
 
-// solution([70, 50, 80, 50], 100);
+solution([70, 50, 80, 50], 100);
 solution([70, 80, 50], 100);
