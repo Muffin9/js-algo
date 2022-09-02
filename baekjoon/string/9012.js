27ba6f6fc71069e05b9047f0b5da5eb759464131
Muffin <jinlog9@gmail.com>
2022-09-02 16:18:39 +0900

    feat: 괄호 문제 스택으로 다시 풀어보기.

--- baekjoon/string/9012.js
+++ baekjoon/string/9012.js
@@ -3,33 +3,25 @@ const input = fs.readFileSync("9012.txt").toString().split("\n");
 
 const solution = () => {
     const N = +input.shift();
-
     for(let i = 0; i < N; i++) {
-        const stack = input.shift().split("");
-        let brackCount = 0;
+        const bracket = input[i].split("");
+        const stack = [];
         let flag = true;
-
-        while(stack.length !== 0) {
-            const bracket = stack.pop();
-            if(bracket === "(") {
-                brackCount -= 1;
-            }
-            else if(bracket === ")") {
-                brackCount += 1;
-            }
-            if(brackCount < 0) {
-                console.log("NO")
+        for(let j = 0; j < bracket.length; j++) {
+            if(stack.length === 0 && bracket[j] === ')') {
                 flag = false;
                 break;
             }
-        }
-
-        if(flag) {
-            if(brackCount === 0){
-                console.log("YES");
+            else if(stack.length !== 0 && bracket[j] === ')') {
+                stack.pop();
             } else {
-                console.log("NO");
+                stack.push('(');
             }
+        }
+        if(flag) {
+            stack.length === 0 ? console.log("YES") : console.log("NO");
+        } else {
+            console.log("NO");
         }
     }
 }
