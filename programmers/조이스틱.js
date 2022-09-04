33eba0447bb7140045439b0a43398ac818ad107c
Muffin <jinlog9@gmail.com>
2022-09-04 19:54:17 +0900

    feat: 조이스틱 문제 풀어보고 이해하기

--- programmers/조이스틱.js
+++ programmers/조이스틱.js
@@ -1,19 +1,20 @@
 function solution(name) {
     let answer = 0;
-    const arr = new Array(name.length);
-    let move = name.length - 1;
-    for(let i = 0; i < name.length; i++) {
-        answer += Math.min(91 - name[i].charCodeAt(0), name[i].charCodeAt(0) - 65);
-        if (i < name.length - 1 && name[i].charCodeAt(i) === 65) {
-            const count = i + 1;
-            while(count < name.length && name[i].charCodeAt(count) === 65) count += 1;
 
-            move = Math.min(move, i * 2 + (name.length - count));
-            move = Math.min(move, i + (name.length - count) * 2);
-        }
+    // 제일 짧은 좌, 우 이동은 그냥 맨 오른쪽으로 이동할 때
+    const len = name.length;
+    let move = len - 1;
+
+    for(let i = 0; i < len; i++) {
+        answer += Math.min(91 - name[i].charCodeAt(0), name[i].charCodeAt(0) - 65);
+        let nextIdx = i + 1;
+
+        while (nextIdx < len && name[nextIdx].charCodeAt(0) === 65) nextIdx++;
+        move = Math.min(move, i + len - nextIdx + Math.min(i, len - nextIdx));
     }
+
     console.log(answer, move);
     return answer + move;
 }
 
-solution("JAN");
+solution("ZHEAAAAAABC");
