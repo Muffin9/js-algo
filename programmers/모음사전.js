4c83f54fa579aaca47b66e02a5bb4d87482a0eb0
Muffin <jinlog9@gmail.com>
2022-09-05 15:52:52 +0900

    [PG]: 완전탐색 - 모음사전 공간복잡도를 줄이는 풀이.

--- programmers/모음사전.js
+++ programmers/모음사전.js
@@ -1,24 +1,37 @@
+// function solution(word) {
+//     const words = ['A', 'E', 'I', 'O', 'U'];
+
+//     const arr = [];
+//     const dfs = (str, len) => {
+//         if(len === 5) return;
+//         arr.push(str);
+//         if(str === word) {
+//             return arr.indexOf(str) + 1;
+//         }
+//         for(let i = 0; i < words.length; i++) {
+//             dfs(str + words[i], len + 1);
+//         }
+//     }
+
+//     for(let i = 0; i < words.length; i++) {
+//         dfs(words[i], 0);
+//     }
+
+//     return arr.indexOf(word) + 1;
+// }
+
+
 function solution(word) {
-    const words = ['A', 'E', 'I', 'O', 'U'];
+    const wordOfRate = [781, 156, 31, 6, 1];
+    const words = ["A", 'E', 'I', 'O', 'U'];
+    let answer = word.length;
 
-    const arr = [];
-    const dfs = (str, len) => {
-        if(len === 5) return;
-        arr.push(str);
-        if(str === word) {
-            return arr.indexOf(str) + 1;
-        }
-        for(let i = 0; i < words.length; i++) {
-            dfs(str + words[i], len + 1);
-        }
+    for(let i = 0; i <  word.length; i++) {
+        console.log(wordOfRate[i], words.indexOf(word[i]));
+        answer += wordOfRate[i] * words.indexOf(word[i]);
     }
-
-    for(let i = 0; i < words.length; i++) {
-        dfs(words[i], 0);
-    }
-
-    return arr.indexOf(str) + 1;
+    console.log(answer);
 }
 
-
 solution('AAAAE');
+solution('I')
