7ae774fe7a4b4e9cf1a9ff76b59a789dbc245678
muffin9 <jinlog9@gmail.com>
2023-01-10 16:32:31 +0900

    [PG] 게임맵 최단거리 다시 풀어보기

--- programmers/게임맵최단거리.js
+++ programmers/게임맵최단거리.js
@@ -5,40 +5,34 @@ function solution(maps) {
     const visited = Array.from({length: n}, () => new Array(m).fill(0));
 
     const moveX = [-1, 1, 0, 0];
-    const moveY = [0, 0, -1, 1];
-
-    const checkBlock = (x, y) => {
-        if(x >= 0 && x < n && y >= 0 && y < m) return true;
-        return false;
-    }
+    const moveY = [0, 0, 1, -1];
 
     const bfs = (x, y) => {
-        const queue = [{x, y}];
-        visited[x][y] = true;
-
+        const queue = [[x, y]];
+        visited[x][y] = 1;
+        
         while(queue.length) {
-            const {x, y} = queue.shift();
+            const [x, y] = queue.shift();
 
             for(let i = 0; i < 4; i++) {
                 const nextX = x + moveX[i];
                 const nextY = y + moveY[i];
 
-                if(!checkBlock(nextX, nextY)) continue;
+                if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
 
                 if(!visited[nextX][nextY] && maps[nextX][nextY]) {
                     visited[nextX][nextY] = visited[x][y] + 1;
-                    queue.push({x: nextX, y: nextY});
+                    queue.push([nextX, nextY]);
                 }
             }
         }
     }
 
     bfs(0, 0);
-    if(visited[n - 1][m - 1] === 0) return -1;
-    return visited[n - 1][m - 1];
-}   
+    return visited[n - 1][m - 1] === 0 ? -1 : visited[n - 1][m - 1]
+}
 
 
 const maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
-const maps2 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]];
-solution(maps2);
+// const maps2 = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]];
+solution(maps);
