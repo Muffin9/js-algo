fcd02b06e39a16cadc494c83d2d4990f81e474ed
muffin9 <jinlog9@gmail.com>
2023-01-25 20:43:06 +0900

    [BG] 누적합 문제 하나 풀기

--- baekjoon/cumulativeSum/구간합구하기4_11659.js
+++ baekjoon/cumulativeSum/구간합구하기4_11659.js
@@ -1,23 +1,26 @@
-const fs = require('fs');
-const [input, nums, ...arr] = fs.readFileSync('11659.txt').toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync("11659.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
 const solution = () => {
-    const [N, M]  = input.split(" ").map(v => +v);
-    const numArr = nums.split(" ").map(v => +v);
-    const sumArr = new Array(numArr.length + 1).fill(0);
-    const result = [];
+  const [N, M] = input
+    .shift()
+    .split(" ")
+    .map((v) => +v);
+  const numArr = [0].concat(...input.shift().split(" ").map(Number));
+  const result = [];
 
-    for(let i = 0; i < numArr.length; i++) {
-        sumArr[i + 1] = sumArr[i] + numArr[i];
-    }
-    
+  for (let i = 1; i < numArr.length; i++) {
+    numArr[i] += numArr[i - 1];
+  }
 
-    for(let i = 0; i < M; i++) {
-        const [start, end] = arr[i].split(" ").map(v => +v);
-        result.push(sumArr[end] - sumArr[start - 1]);
-    }
-    
-    console.log(result);
-}
+  for (let i = 0; i < input.length; i++) {
+    const [start, end] = input[i].split(" ").map(Number);
+    result.push(numArr[end] - numArr[start - 1]);
+  }
+  console.log(result.join("\n"));
+};
 
 solution();
